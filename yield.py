42dfb04cbd6b8f6775d773257647809c800de57d
MGalganek <galganek.mateusz@gmail.com>
2014-01-17 12:54:02 +0100

    Add support to use mock patch to decorator, but it need also support from side of nose by changing isgenerator function

--- yield.py
+++ yield.py
@@ -10,6 +10,7 @@ from nose.util import (
     isclass,
     isgenerator
 )
+from mock import patch, MagicMock
 
 
 class NewFunctionTestCase(FunctionTestCase):
@@ -32,13 +33,14 @@ class NewFunctionTestCase(FunctionTestCase):
         Run setup function attached to the test function.
         Added setup_mocks which must be run when call mock from inherited class.
         """
-        try_run(self.inst, ('setup_mocks', 'setUp'))
+        # import pdb; pdb.set_trace()
+        try_run(self.inst, ('setup_mocks', 'setup', 'setUp', 'setUpFunc'))
 
     def tearDown(self):
         """
         Added teardown_mocks which must be run to stop all mocks.
         """
-        try_run(self.inst, ('teardown_mocks', 'tearDown'))
+        try_run(self.inst, ('teardown_mocks', 'tearDown', 'setUp', 'setUpFunc'))
 
     def __str__(self):
         """
@@ -63,13 +65,16 @@ class CustomLoaderForGenerators(TestLoader):
     which Lazy-load tests from a generator function.
     """
 
-    def load_tests_from_generator_method_with_set_up(self, generator, cls):
+    def load_tests_from_generator_method_with_set_up(
+            self, generator, cls, mock_patch
+    ):
         """
         Create instance of TestCase to call setUp, which is need to
         prevent error: (No api proxy found for service "datastore_v3)".
         """
         test_class_instance = cls()
         test_class_instance.setUp()
+        test_class_instance.setup_mocks()
 
         def generate():
             """
@@ -77,14 +82,31 @@ class CustomLoaderForGenerators(TestLoader):
             loadTestsFromGeneratorMethod to send 'test_name' of "check_"
             and limit operations to have only those which are required.
             """
-            for test in generator(test_class_instance):
-                test_internal_check, arguments = self.parseGeneratedTest(test)
-                yield NewFunctionTestCase(
-                    test=test_internal_check,
-                    arg=arguments,
-                    cls=cls,
-                    test_name=generator.im_func.__name__
-                )
+            if mock_patch is None:
+                for test in generator(test_class_instance):
+
+                    test_check, arguments = self.parseGeneratedTest(test)
+                    yield NewFunctionTestCase(
+                        test=test_check,
+                        arg=arguments,
+                        cls=cls,
+                        test_name=generator.im_func.__name__
+                    )
+            else:
+                # Temp to prevent pep8 errors
+                decorator_patch = MagicMock()
+                decorator_patch.return_value = ''
+
+                with patch(mock_patch) as decorator_patch:
+                    for test in generator(test_class_instance):
+
+                        test_check, arguments = self.parseGeneratedTest(test)
+                        yield NewFunctionTestCase(
+                            test=test_check,
+                            arg=arguments,
+                            cls=cls,
+                            test_name=generator.im_func.__name__
+                        )
         # Back to built-in nose method
         return self.suiteClass(generate, context=generator, can_split=False)
 
@@ -103,7 +125,11 @@ class YieldWithSetUp(Plugin):
         """
         Only this function from nose API give test (cls) and testCase (cls).
         """
+        mock_patch = None
         if ismethod(obj) and isgenerator(obj) and isclass(cls):
+
+            if hasattr(obj, 'patchings'):
+                mock_patch = get_mock_patch_of_test(obj)
 
             # If generator is found, override loader from nose
             x = CustomLoaderForGenerators()
@@ -111,7 +137,15 @@ class YieldWithSetUp(Plugin):
             # 'return' is required on x when we have generators.
             # This will allow in test function, change to next
             # check function (internal function)
-            return x.load_tests_from_generator_method_with_set_up(obj, cls)
+            return x.load_tests_from_generator_method_with_set_up(
+                obj, cls, mock_patch
+            )
+
+
+def get_mock_patch_of_test(obj):
+    mock_patch = obj.patchings[0].getter().__name__
+    mock_patch += '.' + obj.patchings[0].attribute
+    return mock_patch
 
 if __name__ == '__main__':
     nose.main()
